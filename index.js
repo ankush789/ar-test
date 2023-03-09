9fa77c88e26fc7c48d14242142d0137350ff4ae0
Ankush Sharma <56834051+ankush789@users.noreply.github.com>
2023-03-09 12:43:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+  console.log('clicked');
+  console.log(navigator);
+  navigator.bluetooth
+    .requestDevice({ filters: [{ services: ['battery_service'] }] })
+    .then((device) => {
+      console.log(`Name: ${device.name}`);
+      // Do something with the device.
+    })
+    .catch((error) => console.error(`Something went wrong. ${error}`));
+});
